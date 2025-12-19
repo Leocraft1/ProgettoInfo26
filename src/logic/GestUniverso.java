64717422217cc57d6bc3975917e6f9a486e5e984
Leocraft1 <leonardotoschi07@gmail.com>
2025-12-19 12:00:22 +0100

    Created GestUniverso

---
+++ src/logic/GestUniverso.java
@@ -0,0 +1,11 @@
+package logic;
+
+import java.util.ArrayList;
+
+import leolib.iodb.DBConnector;
+
+public class GestUniverso {
+	private DBConnector dbc;
+	private ArrayList<String> tab_names;
+	private ArrayList<ArrayList<String>> tab_attr;
+}
