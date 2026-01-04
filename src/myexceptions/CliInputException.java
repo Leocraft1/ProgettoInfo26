ac8e127238c582a59849499acf669636dc2f0fab
loren <loren@Lorenzo-LAPTOP.Home>
2026-01-04 15:57:39 +0100

    Allora ho fatto una nuova classe in cli, come mi avevi detto tu e l'ho chiamata CliUniverso, così non so magari è più semplice da correggere... Poi ho aggiunto delle nuovi classi in myexceptions per appunto tutte le eccezzioni e credo che aver fatto tutti i controlli del caso, se ci sono da aggiungere dimmelo che lo rifaccio meglio! Per le eccezzioni ti ricordo che io ho sempre usato super perché è molto più comodo ma se vuoi che faccia le cose come la prof, le rifaccio come vuole la prof diciamo! Ah giusto e poi ho anche aggiunto in CliUniverso delle cose che possono essere utili anche se non me le avevi chieste GUARDA SONO SICURO CHE TU LE AVESSI GIA' MESSE NELLA LIBREARIA MA NON AVEVO VOGLIA DI CERCARLE TRA LE MILIARDI DI PACKAGE QUINDI BEH VABBE' AL MASSIMO LE CANCELLO!

---
+++ src/myexceptions/CliInputException.java
@@ -0,0 +1,8 @@
+package myexceptions;
+
+public class CliInputException extends Exception {
+
+    public CliInputException(String message) {
+        super(message);
+    }
+}
---
+++ src/myexceptions/DuplicateException.java
@@ -0,0 +1,8 @@
+package myexceptions;
+
+public class DuplicateException extends Exception {
+
+    public DuplicateException(String message) {
+        super(message);
+    }
+}
